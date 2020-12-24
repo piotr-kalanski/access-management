2adf770abeabd4395d4e9c474bb63ffc0dcc3012
piotr-kalanski <piotr.kalanski@gmail.com>
2020-12-24 22:48:28 +0100

    layout with material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import Connections from './containers/connections/Connections';
 import Datasets from './containers/datasets/Datasets';
+import Users from './containers/users/Users';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route path="/connections" component={Connections} />
           <Route path="/datasets" component={Datasets} />
+          <Route path="/users" component={Users} />
         </Switch>
       </Layout>
     </BrowserRouter>
--- frontend/src/hoc/Layout/Toolbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Toolbar = () => {
-    return (
-        <p>"TODO - toolbar"</p>
-    )
-}
-
-export default Toolbar;
